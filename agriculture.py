c58d094aa29677218be3496389fd839963e4a7ee
Joseph Wakahu Ngacha <83699448+dynamic2code@users.noreply.github.com>
2023-01-18 22:03:00 +0300

    changes

--- agriculture.py
+++ agriculture.py
@@ -1,3 +1,26 @@
 
-location = input('Enter your location: ')
-land_size = input('Enter your land size in Acers: ')
+
+class Getdata:
+    def __init__(self,location, landSize, income) -> None:
+        self.location = location
+        self.landSize = landSize
+        self.income = income
+
+    def getuserinput(self):
+        self.location = input('Enter your location (give string): ')
+        self.landSize = int(input('Enter your land size in Acers (give a number): '))
+        self.income = int(input('Enter your estimated income per year: '))
+        
+        # verifying the input to avoid errors 
+
+        try:
+            type(self.location) == str 
+            type(self.landSize) == int
+            type(self.income)   == int
+        except:
+            wrongInput = "Your input were out of range. \n Ensure your input marches with the direction"
+
+            print(wrongInput)
+    
+    def getInfoWithAPI():
+        pass
