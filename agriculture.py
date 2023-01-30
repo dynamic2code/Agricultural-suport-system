130cd239db79eea2d72ac5a3c3da578da58039c1
Joseph Wakahu Ngacha <83699448+dynamic2code@users.noreply.github.com>
2023-01-30 16:27:36 +0300

    more

--- agriculture.py
+++ agriculture.py
@@ -14,6 +14,7 @@ class Getdata:
         # verifying the input to avoid errors 
 
         try:
+            
             type(self.location) == str 
             type(self.landSize) == int
             type(self.income)   == int
@@ -21,8 +22,20 @@ class Getdata:
             wrongInput = "Your input were out of range. \n Ensure your input marches with the direction"
 
             print(wrongInput)
-    
+
+class GetWetherdata():
+    # return the wether condition 
+    def guageClimate():
+        pass
     def getInfoWithAPI():
         # use the location to get the wether soil and any other needed conditions
         # wether =
         pass
+class CompareWithCropRequirments():
+     pass
+
+class EconomicFactor():
+    pass
+
+class BestPracticeActivities():
+    pass
