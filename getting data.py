27c4301b6c442141a2fd7150c3f1e4124315c8ef
Joseph Wakahu Ngacha <83699448+dynamic2code@users.noreply.github.com>
2023-02-02 16:14:29 +0300

    additional files

---
+++ getting data.py
@@ -0,0 +1,19 @@
+import csv
+
+class GettingData:
+    def __init__(self):
+        self.file = "kenya-climate-data-1991-2016-rainfallmm.csv"
+    
+    def extractingData(self):
+        with open(self.file, 'r') as file:
+            rows = csv.reader(file)
+            for row in rows:
+                year = row[0]
+                month = row[1]
+                monthlyAvarage = row[2]
+        print(month)
+        print("he")
+
+
+obj = GettingData()
+obj.extractingData
---
+++ tea.py
@@ -0,0 +1,9 @@
+import csv
+def extractingData():
+    included_cols = [0,1,2]
+    with open("kenya-climate-data-1991-2016-rainfallmm.csv", 'r') as file:
+        rows = csv.reader(file)
+
+ 
+
+extractingData()
