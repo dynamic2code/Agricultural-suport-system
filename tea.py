e036ecb06bbfa2e781931b2a7e90e5f69b856768
Joseph Wakahu Ngacha <83699448+dynamic2code@users.noreply.github.com>
2023-02-09 22:14:19 +0300

    for the culture

--- tea.py
+++ tea.py
@@ -3,11 +3,6 @@ import csv
 
 
 def extracting_data():
-    # file = "kenya-climate-data-1991-2016-rainfallmm.csv"
-    # data = pd.read_csv(file)
-    # # mean for all the years will be
-    # mean_for_location = data["Rainfall - (MM)"].mean()
-    # print(mean_for_location)
 	file = "kenya-climate-data-1991-2016-rainfallmm.csv"
 	data = pd.read_csv(file)
 	mean_for_location = data['Rainfall - (MM)'].mean()
@@ -15,19 +10,22 @@ def extracting_data():
 	years =[]
 	months = []
 	
-	with open(file,'r'):
-		rows = csv.reader(file)
+	with open(file,'r') as f:
+		rows = csv.reader(f)
+		
 		for row in rows:
+			print(row[0])
 			if row[0] not in years:
 				years.append(row[0])
 				
 			if row[1] not in months:
 				months.append(row[1])
 				
-	years.pop(0)
-	months.pop(0)
+	months.pop(0)			
+	years.pop(0)			
 	print("This are the unique years",years)
 	print("this are the distinct months",months)
+	print("for the culture")
 							
     # grouping the data by years to get the mean per year and get the climatic progression
     # years = data.groupby("Year").count()
