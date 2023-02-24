043d787b5cd6930b7946b6cf2a172f7b0184c0d0
Joseph Wakahu Ngacha <83699448+dynamic2code@users.noreply.github.com>
2023-02-24 08:51:43 +0300

    working

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-import data_patterns
 import pandas as pd
 import csv
 import requests
 import secret
 
+
 def extracting_data():
-   
+    location = input("enter your location: ")
+    StartDate = ''
+    EndDate = ''
+    response = requests.get(f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}?unitGroup=metric&include=days&key={secret.API_KEY}&contentType=csv')
+    print(response.status_code)
+
+
 extracting_data()
