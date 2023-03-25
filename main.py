59b5ff8019413afdb895e23710945da5755f91ec
dynamic2code <dynamicdesingns@gmail.com>
2023-03-25 10:57:16 +0300

    changes to include new model

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import secret
 from io import StringIO
 import urllib.request
 import sys
+import joblib
 
 import csv
 import codecs
@@ -12,7 +13,7 @@ def extracting_data():
     #weater data from timeline wether Api
     #get worning data from https://www.appsforagri.com/ it will help in comparing the needed condition and the current one
     location = input("enter your location: ")
-    startDate = '2020-6-5'
+    startDate = '2022-6-5'
     endDate = '2023-1-1'
     # response_future_weather = f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}?unitGroup=metric&include=days&key={secret.API_KEY}&contentType=csv'
     # response_past_weather = f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}/{startDate}/{endDate}?unitGroup=metric&include=days&key={secret.API_KEY}&contentType=csv'
@@ -39,7 +40,7 @@ def extracting_data():
     # weather soil temp, soil moisture, evaporation, wind speed, wind direction, direct normal radiation
     try:
         ResultBytes = urllib.request.urlopen(
-            "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Nairobi/last30days?unitGroup=metric&elements=windspeed50%2Cwinddir50%2Cdniradiation%2Csoiltemp04%2Csoilmoisture04%2Cet0&include=days&key=YOUR_API_KEY&contentType=csv")
+            f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Nairobi/last30days?unitGroup=metric&elements=windspeed50%2Cwinddir50%2Cdniradiation%2Csoiltemp04%2Csoilmoisture04%2Cet0&include=days&key={secret.API_KEY}&contentType=csv")
 
         # Parse the results as CSV
         CSVText = csv.reader(codecs.iterdecode(ResultBytes, 'utf-8'))
@@ -53,9 +54,8 @@ def extracting_data():
         print('Error code: ', e.code, ErrorInfo)
         sys.exit()
 
-
-
-
+        X_new = [[...], [...], ...]
+    return x_new
     #crop recomendation dataset from kaggel https://www.kaggle.com/datasets/atharvaingle/crop-recommendation-dataset?resource=download
     # N - ratio of Nitrogen content in soil
     # P - ratio of Phosphorous content in soil
@@ -64,6 +64,14 @@ def extracting_data():
     # humidity - relative humidity in %
     # ph - ph value of the soil
     # rainfall - rainfall in mm
+def crop_recomendation(X_new):
+    # load the saved model
+    model = joblib.load('crop_prediction.h5')
+    # make predictions on new data
+    #X_new =   new data as a list of feature vectors
+    y_pred = model.predict(X_new)  # predicted target values
 
+    return y_pred
 
 extracting_data()
+
